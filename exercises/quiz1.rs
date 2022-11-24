33ee0b0171e716ea2bd4f9a434abcf69d6f66b15
njzy <njzydark@gmail.com>
2022-11-24 18:16:03 +0800

    feat: quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: u32) -> u32 {
+    let mut price: u32 = 2;
+    if (count > 40) {
+        price = 1;
+    }
+    return count * price;
+}
 
 // Don't modify this function!
 #[test]
